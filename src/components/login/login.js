510eb6fe456d2b758cb5773bd8612a7bf2cc0c42
Per Eskilson <paelsis@hotmail.com>
2020-11-03 21:08:53 +0100

    Nicer login screen

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,51 +1,71 @@
-import React from "react"
+import React, {useState} from "react"
 import { navigate } from "gatsby"
 import { handleLogin, isLoggedIn } from "../../services/auth"
 
-class Login extends React.Component {
-  state = {
-    username: ``,
-    password: ``,
+export default () => {
+  const [user, setUser] = useState({})  
+  const handleChange = e => {
+    setUser({...user, [e.target.name]: e.target.value})
   }
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    })
+  const handleChecked = e => {
+    setUser({...user, [e.target.name]: e.target.checked})
   }
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
-    handleLogin(this.state)
+    handleLogin(user)
   }
-  render() {
-    if (isLoggedIn()) {
+  if (isLoggedIn()) {
       navigate(`/app/profile`)
-    }
-    return (
-      <>
-        <h1>Log in</h1>
-        <form
-          method="post"
-          onSubmit={e => {
-            this.handleSubmit(e)
-            navigate(`/app/profile`)
-          }}
-        >
-          <label>
-            Username
-            <input type="text" name="username" onChange={this.handleChange} />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              name="password"
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Log In" />
-        </form>
-      </>
-    )
   }
+  return (
+        <section class="hero is-halfheight">
+        <div class="hero-body">
+            <div class="container is-primary">
+            <div className="columns is-centered">
+                <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+                <form 
+                    className="box"
+                    style={{backgroundColor:'lightGreen', opacity:0.9}}                        
+                    method="post"
+                    onSubmit={e => {
+                        handleSubmit(e)
+                        navigate(`/app/profile`)
+                    }}
+                >
+                    <div class="field">
+                    <label for="" class="label">Username</label>
+                    <div class="control has-icons-left">
+                        <input type="text" placeholder="e.g. john" class="input" name="username" onChange={handleChange} required />
+                        <span class="icon is-small is-left">
+                        <i class="fa fa-envelope"></i>
+                        </span>
+                    </div>
+                    </div>
+                    <div class="field">
+                    <label for="" class="label">Password</label>
+                    <div class="control has-icons-left">
+                        <input type="password" placeholder="*******" class="input" name="password" onChange={handleChange} required />
+                        <span class="icon is-small is-left">
+                        <i class="fa fa-lock"></i>
+                        </span>
+                    </div>
+                    </div>
+                    <div class="field">
+                    <label for="" class="checkbox">
+                        <input type="checkbox" name="checkbox" onChange={handleChecked} />
+                        Remember me
+                    </label>
+                    </div>
+                    <div class="field">
+                    <button class="button is-success" style={{backgroundColor:'green'}}>
+                        Login
+                    </button>
+                    </div>
+                </form>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+    )
 }
-export default Login
